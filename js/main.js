e9e2960916ca194770c510069ad5e4d9faeeae67
David <itsdavid71@gmail.com>
2022-07-13 12:37:25 +0300

    Up

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
 function init() {
   //   new Splide(".splide").mount({});
 
+  // Menu
+  $(".menu-button").click(function () {
+    $(".header-responsive-menu").slideToggle(300);
+    $(this).toggleClass("open");
+  });
+  $(".nav-link").click(function () {
+    $(".header-responsive-menu").slideUp(300);
+    $(".menu-button").removeClass("open");
+  });
+
   $(".members-js").slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 2,
     rows: 2,
+    dots: true,
+
     responsive: [
       {
         breakpoint: 992,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
+          arrows: false,
           infinite: true,
           dots: true,
         },
@@ -21,6 +34,7 @@ function init() {
         settings: {
           dots: true,
           slidesToShow: 2,
+          arrows: false,
           slidesToScroll: 2,
         },
       },
@@ -28,6 +42,7 @@ function init() {
         breakpoint: 480,
         settings: {
           dots: true,
+          arrows: false,
           slidesToShow: 1,
           slidesToScroll: 1,
           rows: 1,
@@ -37,6 +52,19 @@ function init() {
       // settings: "unslick"
       // instead of a settings object
     ],
+  });
+
+  $("a").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top + "px",
+      },
+      {
+        duration: 500,
+        easing: "swing",
+      }
+    );
+    return false;
   });
 }
 
